2757186a8f9e843767b1e00243734d177c045d76
Baudoint Emma <baudoint@insa-20670>
2021-03-10 10:49:25 +0100

    add watchdog

--- software/raspberry/superviseur-robot/tasks.h
+++ software/raspberry/superviseur-robot/tasks.h
@@ -132,6 +132,11 @@ private:
      */
     void MoveTask(void *arg);
     
+    /**
+     * @brief handling start with watchdog.
+     */
+    void WatchDog(void *arg);
+    
     /**********************************************************************/
     /* Queue services                                                     */
     /**********************************************************************/
